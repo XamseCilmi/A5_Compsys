9e48e57a95b5f13403b115ac28366f176af001a5
XamseCilmi <XamseCilmi@github.com>
2023-12-14 08:32:06 +0100

    added main.c to test

--- src/test/main.c
+++ src/test/main.c
@@ -7,11 +7,5 @@
 #include <string.h>
 #include <time.h>
 int main(int argc, char *argv[]){
-FILE *log_file = NULL;
-log_file = fopen(argv[2], "w");
-struct memory *mem = memory_create();
-struct assembly *as = assembly_create();
-int start_addr = read_exec(mem, as, argv[1], log_file);
-printf("start: %d, argc: %d\n",start_addr, argc);
-return 0;
+
 }
